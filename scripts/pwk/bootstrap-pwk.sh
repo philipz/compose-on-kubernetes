6368e267f17b71700b3af843fb3ae8366edf76a0
philipz <philipzheng@gmail.com>
2019-02-19 13:15:26 +0000

    Modify bootstrap-pwk.sh for K8S playground.

--- scripts/pwk/bootstrap-pwk.sh
+++ scripts/pwk/bootstrap-pwk.sh
@@ -2,6 +2,8 @@
 
 kubeadm init --apiserver-advertise-address $(hostname -i)
 mkdir -p $HOME/.kube
+mkdir -p $HOME/.kube
+cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
 chown $(id -u):$(id -g) $HOME/.kube/config
 kubectl apply -n kube-system -f \
     "https://cloud.weave.works/k8s/net?k8s-version=$(kubectl version | base64 |tr -d '\n')"
